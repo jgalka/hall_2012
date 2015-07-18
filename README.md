3c583764646fc6085d4872cac9644aa6b209509c
JGalka <jacek.galka@wp.pl>
2015-07-18 22:44:44 +0200

    wrzutka zaleglości

---
+++ README.md
@@ -0,0 +1,21 @@
+##O projekcie:
+
+Podział Pracy (timeline):
+- Dodanie komentarzy do produktów: 
+   - Komentarze bez rejestracji / kod recaptcha badz cos podobnego
+- Design
+
+##Linki: 
+Git podstawowa obsługa ref: https://github.com/rails4/hall2012/wiki/Praca-z-Gitem
+
+##Team:
+
+M. Bryl - wiki/git/design strony/branch_master
+
+R. Byczkowski - lokalizacja/tłumaczenie
+
+J. Darecki - serwer/konfiguracja/postawienie
+
+J. Gałka - ?
+
+A. Karpiński - dodanie komentarzy
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hall2012::Application.config.secret_token = '88ae4e7bed962e72d00e4575fb9090a58a8131bf4b4778a48d15eebb437573a7a4b9090a4b039d1e9b186c658402196f74001e92eebc715938573fdf9c184479'
